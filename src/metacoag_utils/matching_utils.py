Fix poisson PMF calculation and variables

src/metacoag_utils/matching_utils.py
@@ -3,10 +3,10 @@
 import math
 
 from scipy.spatial import distance
-from scipy.stats import poisson
 
-mu_intra, sigma_intra = 0, 0.01037897 / 2
-mu_inter, sigma_inter = 0.0676654, 0.03419337
+MU_INTRA, SIGMA_INTRA = 0, 0.01037897 / 2
+MU_INTER, SIGMA_INTER = 0.0676654, 0.03419337
+VERY_SMALL_DOUBLE = 1e-200
 
 
 def normpdf(x, mean, sd):
@@ -21,8 +21,8 @@ def get_tetramer_distance(seq1, seq2):
 
 
 def get_comp_probability(tetramer_dist):
-    gaus_intra = normpdf(tetramer_dist, mu_intra, sigma_intra)
-    gaus_inter = normpdf(tetramer_dist, mu_inter, sigma_inter)
+    gaus_intra = normpdf(tetramer_dist, MU_INTRA, SIGMA_INTRA)
+    gaus_inter = normpdf(tetramer_dist, MU_INTER, SIGMA_INTER)
     return gaus_intra/(gaus_intra+gaus_inter)
 
 
@@ -31,6 +31,18 @@ def get_cov_probability(cov1, cov2):
     poisson_prod = 1
 
     for i in range(len(cov1)):
-        poisson_prod = poisson_prod * poisson.pmf(cov1[i], cov2[i])
+
+        if cov1[i]!=0.0 and cov2[i]!=0.0:
+
+            # Adapted from http://www.masaers.com/2013/10/08/Implementing-Poisson-pmf.html
+            poisson_pmf = math.exp((cov1[i] * math.log(cov2[i])) - math.lgamma(cov1[i] + 1.0) - cov2[i])
+
+            if poisson_pmf < VERY_SMALL_DOUBLE:
+                poisson_pmf = VERY_SMALL_DOUBLE
+            
+            poisson_prod = poisson_prod * poisson_pmf
+
+    if poisson_prod == 1:
+        poisson_prod = VERY_SMALL_DOUBLE
     
     return poisson_prod