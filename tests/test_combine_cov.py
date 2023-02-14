6075e5d643a03a154fa1024dcc5ff0dfb2804df5
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-02-14 22:27:12 +1030

    TST: Add combine_cov tests

---
+++ tests/test_combine_cov.py
@@ -0,0 +1,52 @@
+import subprocess
+
+from pathlib import Path
+
+import pytest
+
+
+__author__ = "Vijini Mallawaarachchi and Yu Lin"
+__copyright__ = "Copyright 2020, MetaCoAG Project"
+__license__ = "GPL-3.0"
+__version__ = "1.1.0"
+__maintainer__ = "Vijini Mallawaarachchi"
+__email__ = "vijini.mallawaarachchi@anu.edu.au"
+__status__ = "Development"
+
+
+TEST_ROOTDIR = Path(__file__).parent
+EXEC_ROOTDIR = Path(__file__).parent.parent
+
+
+@pytest.fixture(scope="session")
+def tmp_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp("tmp")
+
+
+@pytest.fixture(autouse=True)
+def workingdir(tmp_dir, monkeypatch):
+    """set the working directory for all tests"""
+    monkeypatch.chdir(tmp_dir)
+
+
+def exec_command(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
+    """executes shell command and returns stdout if completes exit code 0
+    Parameters
+    ----------
+    cmnd : str
+      shell command to be executed
+    stdout, stderr : streams
+      Default value (PIPE) intercepts process output, setting to None
+      blocks this."""
+
+    proc = subprocess.Popen(cmnd, shell=True, stdout=stdout, stderr=stderr)
+    out, err = proc.communicate()
+    if proc.returncode != 0:
+        raise RuntimeError(f"FAILED: {cmnd}\n{err}")
+    return out.decode("utf8") if out is not None else None
+
+def test_combine_cov(tmp_dir):
+    """test metacoag on megahit assembly"""
+    dir_name = TEST_ROOTDIR / "data" / "combine_cov_data"
+    cmd = f"combine_cov --covpath {dir_name} --output {tmp_dir}"
+    exec_command(cmd)
