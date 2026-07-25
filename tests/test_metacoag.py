faee144e2b60918677d5d76a5508f6c048b76b9c
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2026-07-25 21:51:49 +0930

    TST: update tests

--- tests/test_metacoag.py
+++ tests/test_metacoag.py
@@ -1,8 +1,10 @@
 import os
 import subprocess
+
 from pathlib import Path
 
 import pytest
+
 
 __author__ = "Vijini Mallawaarachchi"
 __credits__ = ["Vijini Mallawaarachchi"]
@@ -71,7 +73,7 @@ def test_n_bins_metacoag_spades(test_metacoag_spades_command):
     assert n_bins == 5
 
     # Assert bin sizes
-    assert seq_counts == [10, 22, 48, 69, 78]
+    assert seq_counts == [10, 23, 48, 69, 78]
 
 
 @pytest.fixture(scope="session")
@@ -99,4 +101,4 @@ def test_n_bins_metacoag_megahit(test_metacoag_megahit_command):
     assert n_bins == 5
 
     # Assert bin sizes
-    assert seq_counts == [35, 40, 46, 84, 127]
+    assert seq_counts == [36, 40, 46, 84, 127]
